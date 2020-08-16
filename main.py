c37b443efbd0a70de2f05b245a98f98e4ef43093
CheAle14 <thedark1513@gmail.com>
2020-08-16 12:58:23 +0100

    Fix subreddit

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def load_reddit():
     global reddit, subReddit, author
     author = "DarkOverLordCO"
     reddit = praw.Reddit("bot1", user_agent="script:mlapiOCR:v0.0.3 (by /u/" + author + ")")
-    subReddit = reddit.subreddit("mlapi")
+    subReddit = reddit.subreddit("DiscordApp")
 def load_scams():
     global SCAMS, THRESHOLD
     SCAMS = []
