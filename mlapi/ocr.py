78b72bf2fbf28efa42f006e2302735f5edc5a125
CheAle14 <12370876+CheAle14@users.noreply.github.com>
2022-08-31 15:55:58 +0100

    Update ocr.py

--- mlapi/ocr.py
+++ mlapi/ocr.py
@@ -5,17 +5,35 @@ except ImportError:
     import Image
 import pytesseract
 import cv2
-import os, tempfile
+import os, tempfile, numpy
+from colorsys import rgb_to_hls, hls_to_rgb
 if os.name == 'nt':
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
 
+def processImage(image):
+    avg_color_per_row = numpy.average(image, axis=0)
+    avg_color = numpy.average(avg_color_per_row, axis=0) # Blue Green Red.
+    hls = rgb_to_hls(avg_color[2], avg_color[1], avg_color[0])
+    logging.info("Lightness of image: " + str(hls[1])) # between 255 (light) and 0 (dark)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+    # Tesseract works best with dark text on a light background.
+    # So, if the image is (on average) dark we can try and invert it.
+    if hls[1] < 80:
+        logging.info("Inverting image.")
+        gray = cv2.bitwise_not(gray)    
+
+    return gray
 
 def getTextFromPath(path, filename):
     image = cv2.imread(path, cv2.IMREAD_COLOR)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = cv2.bitwise_not(gray)
+
+    processed = processImage(image)
+    
     filename = "corrected_{}.png".format(filename)
     correctedPath = os.path.join(tempfile.gettempdir(), filename)
-    cv2.imwrite(correctedPath, gray)
+    logging.info("Corrected -> " + correctedPath)
+    cv2.imwrite(correctedPath, processed)
     return pytesseract.image_to_string(Image.open(correctedPath))
 
