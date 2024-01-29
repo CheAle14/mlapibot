327e67b7bf06389dd5606a7e1771ac3529c37a6e
CheAle14 <12370876+CheAle14@users.noreply.github.com>
2024-01-29 16:21:44 +0000

    Use sift for scaling

--- mlapi/models/scams/imgscam.py
+++ mlapi/models/scams/imgscam.py
@@ -1,3 +1,4 @@
+import glob
 import os
 from typing import Dict, List
 
@@ -26,27 +27,87 @@ class ImgScamChecker(BaseScamChecker):
             json.get("img")
         )
     
-    def _try_template(self, context: ScamContext, name) -> bool:
-        path = os.path.join(context.data.data_dir, "images", name)
-        template = cv.imread(path, cv.IMREAD_COLOR)
-        assert template is not None, f"template image could not be read at {path}"
-        w, h, cd = template.shape
 
+
+    def _sift_feature_match(self, context, name, img1, img2):
+        MIN_MATCH_COUNT = 10
+        # Initiate SIFT detector
+        sift = cv.SIFT_create(contrastThreshold=0.03, edgeThreshold=20)
+        # find the keypoints and descriptors with SIFT
+        kp1, des1 = sift.detectAndCompute(img1,None)
+        kp2, des2 = sift.detectAndCompute(img2,None)
+        FLANN_INDEX_KDTREE = 1
+        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        search_params = dict(checks = 50)
+        flann = cv.FlannBasedMatcher(index_params, search_params)
+        matches = flann.knnMatch(des1,des2,k=2)
+        # store all the good matches as per Lowe's ratio test.
+        good = []
+        for m,n in matches:
+            if m.distance < 0.7*n.distance:
+                good.append(m)
+        if len(good)>=MIN_MATCH_COUNT:
+            src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+            dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+            M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC,5.0)
+            matchesMask = mask.ravel().tolist()
+            h,w = img1.shape
+            pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+            dst = cv.perspectiveTransform(pts,M)
+            img2 = cv.polylines(img2,[np.int32(dst)],True,255,3, cv.LINE_AA)
+        else:
+            print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
+            matchesMask = None
+            dst = None
+
+        # draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+        #            singlePointColor = None,
+        #            matchesMask = matchesMask, # draw only inliers
+        #            flags = 2)
+        #img3 = cv.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+        #cv.imwrite(f"output_{name}", img3)
+        return np.int32(dst) if dst is not None else None
+
+
+    def _try_template(self, context: ScamContext, path) -> bool:
+        name = os.path.basename(path)
+        if not os.path.exists(path):
+            raise ValueError(f"No image template at {path}")
+        try:
+            img1 = cv.imread(path, cv.IMREAD_GRAYSCALE)
+        except Exception:
+            print(f"Failed to read image template {path}")
+            raise
+        assert img1 is not None, f"template image could not be read at {path}"
+
+        print(">", name)
         for ocr_img in context.images:
-            img = cv.imread(ocr_img.original_path, cv.IMREAD_COLOR).copy()
-            assert img is not None, "file could not be read, check with os.path.exists()"
-            if img.shape[0] < w: continue
-            if img.shape[1] < h: continue
-            # Apply template Matching
-            res = cv.matchTemplate(img, template, cv.TM_CCOEFF_NORMED)
-            # Store the coordinates of matched area in a numpy array 
-            loc = np.where(res >= 0.95) 
-            
-            # Draw a rectangle around the matched region. 
-            for pt in zip(*loc[::-1]): 
-                ocr_img.rectangles.append((pt, (pt[0] + w, pt[1] + h)))
+            img2 = cv.imread(ocr_img.original_path, cv.IMREAD_GRAYSCALE).copy()
+            assert img2 is not None, "file could not be read"
+            rect = self._sift_feature_match(context, name, img1, img2)
+            if rect is not None:
+                rect = [x[0] for x in rect.tolist()]
+                xs = [xy[0] for xy in rect]
+                ys = [xy[1] for xy in rect]
+                rect = [min(xs), min(ys), max(xs), max(ys)]
+                print(rect)
+                ocr_img.rectangles.append(rect)
                 return True
         return False
 
+    def _get_paths(self, context: ScamContext) -> List[str]:
+        paths = []
+        for name in self.img_names:
+            path = os.path.join(context.data.data_dir, "images", name)
+            if '*' in name:
+                for path in glob.glob(path):
+                    if path not in paths:
+                        paths.append(path)
+            else:
+                if path not in paths:
+                    paths.append(path)
+        return paths
+
+        
     def matches(self, context: ScamContext, THRESHOLD: float) -> float:
-        return 1 if any(self._try_template(context, t) for t in self.img_names) else 0
+        return 1 if any(self._try_template(context, t) for t in self._get_paths(context)) else 0
