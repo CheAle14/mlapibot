0a17fada174e93606f9cff09480490fa7996d32a
CheAle14 <12370876+CheAle14@users.noreply.github.com>
2025-03-09 15:31:34 +0000

    fix flags

--- bin/mlapibot-bin/src/reddit.rs
+++ bin/mlapibot-bin/src/reddit.rs
@@ -22,8 +22,8 @@ pub struct RedditArgs {
     #[arg(long, default_value = "false")]
     dry_run: bool,
     /// Whether we are running in production or not
-    #[arg(long, default_value = "true")]
-    debug: bool,
+    #[arg(long)]
+    release: bool,
     /// If present, bind a HTTP listener to the provided address to listen for status webhooks.
     #[arg(long)]
     status_webhook: Option<String>,
@@ -61,7 +61,7 @@ impl RedditArgs {
             dry_run,
             status_webhook,
             admin,
-            debug,
+            release,
         } = self;
 
         let analyzers = mlapibot_analysis::load_scams()?;
@@ -79,7 +79,7 @@ impl RedditArgs {
                 admin,
                 credentials,
                 subreddits_config,
-                debug,
+                !release,
             )?;
 
             client.run()
